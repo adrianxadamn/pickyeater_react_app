b2404ecc981bcaa40b55b435b0237f1350d31796
Adrian Nuyda <adamnuyda@gmail.com>
2019-02-13 22:58:27 -0800

    first commit; adds header and nav components

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Nav from './Nav'; 
+import './styles/header.css';
+
+
+class Header extends Component {
+	render() {
+		return (
+			<section className="header">
+				<div className="logo">
+					<img src="/assets/logo.png" alt="Logo" /> 
+					<Nav />
+				</div>
+			</section>
+		)
+	}
+};
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+import './styles/box-sizing.css';
+import './styles/normalize.css';
+import './styles/reset.css';
+import Header from './Header';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Header />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
